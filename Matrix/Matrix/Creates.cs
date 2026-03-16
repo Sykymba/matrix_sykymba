7cb566445db9bddcd0330f70f55dc79c37357b6b
Sykymba <128389357+Sykymba@users.noreply.github.com>
2026-03-17 00:54:52 +0300

    one_run

---
+++ Matrix/Matrix/Creates.cs
@@ -0,0 +1,21 @@
+﻿namespace MyApp.Utilit;
+
+class Creates
+{
+	// наполнение матрицы
+	public static int[,] create(int[,] matrix, int size)
+	{
+		for (int i = 0 ; i < size; i++)
+		{
+			Console.WriteLine($"Введите строку № {i + 1}");
+			for (int j = 0; j < size; j++)
+			{
+				Console.Write($"     Число №{i + 1}: ");
+				matrix[i, j] = int.Parse(Console.ReadLine());
+			}
+		}
+		
+		return matrix;
+	}
+}
+
---
+++ Matrix/Matrix/info.cs
@@ -0,0 +1,17 @@
+﻿namespace MyApp.Utilit;
+
+class Info
+{
+    public static void info(int[,] matrix,int size)
+    {
+        // вывод матрицы 
+        for (int row = 0; row < size; row++)
+        {
+            for (int coll = 0; coll < size; coll++)
+            {
+                Console.Write($"{matrix[row, coll]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
